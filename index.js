caf01d7a8575f155075edd2fe3be54f119faf0a7
Senai Mogos <smogos797@gmail.com>
2019-01-10 20:01:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,26 +1,17 @@
 // write your code below!
-function happyHolidays(string) {
-  console.log("Happy holidays!")
-  return string
+function happyHolidays() {
+  return ("Happy holidays!") 
 }
 
-function whisper(string) {
-  return string.toLowerCase()
+function happyHolidaysTo(name) {
+  return ('Happy holidays, ${name}!')
+  
 }
 
-function logShout(string) {
-  console.log('hello'.toUpperCase())
-  return logShout
+function happyHolidayTo(holiday, name) {
+  return ('Happy ${holiday}, ${name}!')
 }
 
-function sayHiToGrandma(string) {
-  console.log('HELLO'.toLowerCase()) = console.log("I can\'t hear you!")
-  console.log('hello'.toUpperCase()) = console.log("YES INDEED!")
-  console.log("I love you, Grandma.") = console.log("I love you, too.")
-  return sayHiToGrandma
-}
-
-function logWhisper(string) {
-  console.log('HELLO'.toLowerCase())
-  return logWhisper
+function holidayCountdown(days, holiday) {
+  return ('It\'s ${days} days until ${holiday}!')
 }

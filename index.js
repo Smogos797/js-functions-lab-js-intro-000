3454ade61d93158ca6cb62dca759976327d3b936
Senai Mogos <smogos797@gmail.com>
2019-01-10 19:49:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // write your code below!
+function happyHolidays(string) {
+  console.log("Happy holidays!")
+  return string
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log('hello'.toUpperCase())
+  return logShout
+}
+
+function sayHiToGrandma(string) {
+  console.log('HELLO'.toLowerCase()) = console.log("I can\'t hear you!")
+  console.log('hello'.toUpperCase()) = console.log("YES INDEED!")
+  console.log("I love you, Grandma.") = console.log("I love you, too.")
+  return sayHiToGrandma
+}
+
+function logWhisper(string) {
+  console.log('HELLO'.toLowerCase())
+  return logWhisper
+}
